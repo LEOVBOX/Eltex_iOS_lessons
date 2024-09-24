bdb22937ee52570672b8f7d642dd17df19c366d0
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2024-09-24 20:27:01 +0700

    Initial Commit

---
+++ Eltex_iOS_lessons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eltex_iOS_lessons
+//
+//  Created by Леонид Шайхутдинов on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
